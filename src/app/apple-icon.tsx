2734822e233e79ad0c26f148ad45c5e7a679c236
afonso-resendes <afonsoresendes03@gmail.com>
2025-08-04 14:11:39 +0000

    Remove favicon.ico file to streamline application assets and improve project organization.

---
+++ src/app/apple-icon.tsx
@@ -0,0 +1,40 @@
+import { ImageResponse } from 'next/og'
+
+// Image metadata
+export const size = {
+  width: 180,
+  height: 180,
+}
+export const contentType = 'image/png'
+
+// Image generation
+export default function AppleIcon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          background: 'black',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderRadius: '22px',
+        }}
+      >
+        {/* TupperStock Logo Icon - Larger for Apple devices */}
+        <svg
+          width="120"
+          height="120"
+          viewBox="0 0 20 20"
+          fill="white"
+        >
+          <path d="M3 4a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1V4zM3 10a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H4a1 1 0 01-1-1v-6zM14 9a1 1 0 00-1 1v6a1 1 0 001 1h2a1 1 0 001-1v-6a1 1 0 00-1-1h-2z" />
+        </svg>
+      </div>
+    ),
+    {
+      ...size,
+    }
+  )
+} 
---
+++ src/app/favicon.tsx
@@ -0,0 +1,39 @@
+import { ImageResponse } from 'next/og'
+
+// Image metadata
+export const size = {
+  width: 16,
+  height: 16,
+}
+export const contentType = 'image/x-icon'
+
+// Image generation
+export default function Favicon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          background: 'black',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        {/* TupperStock Logo Icon - Small for favicon */}
+        <svg
+          width="10"
+          height="10"
+          viewBox="0 0 20 20"
+          fill="white"
+        >
+          <path d="M3 4a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1V4zM3 10a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H4a1 1 0 01-1-1v-6zM14 9a1 1 0 00-1 1v6a1 1 0 001 1h2a1 1 0 001-1v-6a1 1 0 00-1-1h-2z" />
+        </svg>
+      </div>
+    ),
+    {
+      ...size,
+    }
+  )
+} 
